29577e4c22471b4f898cc92f7dd15ca82a64be43
DIVYANSHU_INDIA <mail.divyanshuupadhyay@gmail.com>
2023-09-05 18:04:49 +0530

    added Intial Website

--- Script/script.js
+++ Script/script.js
@@ -1,17 +1,21 @@
+//Varibales used in this program.
 let startTime = 0; 
 let interval; 
 let isRunning = false; 
 
+//getting elemenets using id
 const display = document.getElementById('display');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const resetBtn = document.getElementById('resetBtn');
 
+// The date is coming is not in the correct format hence we need to substring those date that is coming from the Date function
 function formatTime(milliseconds) {
     const date = new Date(milliseconds);
     return date.toISOString().substr(11, 8); 
 }
 
+//This function is to start the timer here we are checking if the timmer is running through isRunning function if it is running then 
 function startTimer() {
     if (!isRunning) {
         startTime = Date.now() - (startTime > 0 ? startTime : 0); 
@@ -24,13 +28,14 @@ function startTimer() {
         startBtn.textContent = 'Resume';
     }
 }
-
+//This function is to stop the timmer.
 function stopTimer() {
     clearInterval(interval);
     isRunning = false;
     startBtn.textContent = 'Start';
 }
 
+//reseting the timer function.
 function resetTimer() {
     clearInterval(interval);
     isRunning = false;
@@ -38,7 +43,7 @@ function resetTimer() {
     display.textContent = '00:00:00';
     startBtn.textContent = 'Start';
 }
-
+//This functin is to update the timmer.
 function updateTimer() {
     const currentTime = Date.now() - startTime;
     display.textContent = formatTime(currentTime);
